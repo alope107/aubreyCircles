4b8a584520dda639a0e28d0589fbe0de68a46400
alope107 <auberonlopez@gmail.com>
2024-01-03 19:10:19 -0800

    Basic collisions!

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,13 +4,16 @@
 #include "bn_sprite_items_circle.h"
 #include "bn_keypad.h"
 #include <bn_vector.h>
+#include <bn_memory.h>
 #include <bn_fixed_point.h>
 #include <bn_math.h>
 #include "bn_log.h"
 #include "bn_string.h"
 #include "fixed_point_t.h"
+#include "wake_geometry.h"
 
 const int PRECISION = 19;
+const int MAX_ORBITERS = 200;
 
 using fixed = bn::fixed_t<PRECISION>;
 using fixed_point = circles::fixed_point_t<PRECISION>;
@@ -31,6 +34,11 @@ const Attractor ATTRACTOR = {
     .0001,
 };
 
+fixed squared_distance(fixed_point a, fixed_point b) {
+    fixed_point delta = a - b;
+    return delta.x()*delta.x() + delta.y()*delta.y();
+}
+
 template<int maxSize=10000, typename... Types>
     inline void log(const Types&... vals) {
         // Something bizarre is going on here...
@@ -47,13 +55,18 @@ class Orbiter {
     public:
         fixed_point loc;
         fixed_point velocity;
+        fixed radius;
+        wake::Rectangle bounding;
 
-        Orbiter(fixed_point starting_loc, fixed_point starting_velocity, bn::sprite_item sprite_item, Attractor attractor=ATTRACTOR) :
+        Orbiter(fixed_point starting_loc, fixed_point starting_velocity, fixed sprite_radius, bn::sprite_item sprite_item, Attractor attractor=ATTRACTOR) :
         loc(starting_loc),
         velocity(starting_velocity),
+        radius(sprite_radius),
         _sprite(sprite_item.create_sprite(starting_loc)),
         _attractor(attractor)
-        {};
+        {
+            _update_bounding();
+        };
 
         void update() {
             fixed_point delta = loc - _attractor.location;
@@ -63,18 +76,32 @@ class Orbiter {
             // log("space_location", _space_location.x(), _space_location.y());
             // log("screen_location", screen_location().x(), screen_location().y());
             _sprite.set_position(loc);
+            _update_bounding();
+        }
+
+        bool overlaps(Orbiter other) {
+            log("My Bounding: (", bounding.top_left.x(), ",", bounding.top_left.y(), ")  (",
+                                  bounding.bottom_right.x(), ",", bounding.bottom_right.y(), ")");
+
+            log("Their Bounding: (", other.bounding.top_left.x(), ",", other.bounding.top_left.y(), ")  (",
+                                     other.bounding.bottom_right.x(), ",", other.bounding.bottom_right.y(), ")");
+            return wake::rects_overlap(bounding, other.bounding);
+            // const fixed min_dist = radius + other.radius;
+            // const fixed squared_min_dist = (min_dist * min_dist);
+            // const fixed squared_dist = squared_distance(loc, other.loc);
+            // log("min_dist", min_dist, "squared_min_dist", squared_min_dist, "squared_dist", squared_dist);
+            // return squared_dist <= squared_min_dist;
         }
     
     private:
         bn::sprite_ptr _sprite;
         Attractor _attractor;
-};
 
-fixed scaled_squared_distance(fixed_point a, fixed_point b, fixed s) {
-    fixed_point delta = a - b;
-    delta /= s;
-    return delta.x()*delta.x() + delta.y()*delta.y();
-}
+        void _update_bounding() {
+            bounding = wake::Rectangle(loc - fixed_point(radius, radius),
+                                        loc + fixed_point(radius, radius));
+        }
+};
 
 int main() {
     bn::core::init();
@@ -82,7 +109,9 @@ int main() {
     fixed_point vec_start(0, 0);
 
     bool start_set = false;
-    bn::vector<Orbiter, 200> frems;
+
+    bn::vector<Orbiter, MAX_ORBITERS> frems;
+    bool to_remove[MAX_ORBITERS] = {};
     
     bn::sprite_ptr cursor = bn::sprite_items::circle.create_sprite(30.5, 40.5);
     while(true) {
@@ -91,7 +120,7 @@ int main() {
                 vec_start = cursor.position();
             } else {
                 auto starting_velocity = (fixed_point(cursor.position()) - vec_start) * LAUNCH_SCALE;
-                frems.push_back(Orbiter(vec_start, starting_velocity, bn::sprite_items::circle)); 
+                frems.push_back(Orbiter(vec_start, starting_velocity, 8, bn::sprite_items::circle)); 
             }
 
             start_set = !start_set;
@@ -116,6 +145,23 @@ int main() {
         
         for (auto &frem : frems) {
             frem.update();
+        }
+
+        //bn::memory::clear(MAX_ORBITERS, to_remove);
+
+        for (int i = 0; i < frems.size(); i++) {
+            for (int j = i+1; j < frems.size(); j++) {
+                if(frems[i].overlaps(frems[j])) {
+                    to_remove[i] = to_remove[j] = true;
+                }
+            }
+        }
+
+        for (int i = MAX_ORBITERS - 1; i>= 0; i--) {
+            if(to_remove[i]) {
+                frems.erase(frems.begin() + i);
+            }
+            to_remove[i] = false;
         }
 
         bn::core::update();
